b8520df43b6afcae58845258860ded58ad009faf
shahid <shahidshamimshah3@gmail.com>
2025-03-13 14:56:28 +0530

    backend complated

--- src/routes/profileRouthes.js
+++ src/routes/profileRouthes.js
@@ -1,16 +1,17 @@
 import express from "express";
+import bcrypt from "bcrypt";
 import User from "../models/User.js";
 import Admin from "../models/Admin.js";
 
 const profileRoute = express.Router();
 
-
+// Get user profile
 profileRoute.get("/user/:id", async (req, res) => {
   try {
-    const userId = req.params.id; 
+    const userId = req.params.id;
     let user = await User.findById(userId);
-    if(!user){
-         user = await Admin.findById(userId)
+    if (!user) {
+      user = await Admin.findById(userId);
     }
     if (!user) {
       return res.status(404).json({ message: "User not found" });
@@ -18,6 +19,33 @@ profileRoute.get("/user/:id", async (req, res) => {
     res.json(user);
   } catch (error) {
     console.error("Server error:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Update email and password
+profileRoute.put("/user/:id", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userId = req.params.id;
+    let user = await User.findById(userId);
+    if (!user) {
+      user = await Admin.findById(userId);
+    }
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (email) user.email = email;
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user.password = hashedPassword;
+    }
+    await user.save();
+
+    res.json({ message: "Profile updated successfully" });
+  } catch (error) {
+    console.error("Error updating profile:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
